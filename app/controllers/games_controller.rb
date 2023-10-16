1641c6d6a4456f119e8bbee055ff394e3673f117
jingleville <andrewgavrick@yandex.ru>
2023-10-16 23:17:14 +0300

    added games controller with some actions

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,22 @@
+class GamesController < ApplicationController
+	def index
+		@games = Game.all
+	end
+
+	def show
+		@game = Game.find(params[:id])
+	end
+
+	def new
+		@game = Game.new
+	end
+
+	def create
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+end
